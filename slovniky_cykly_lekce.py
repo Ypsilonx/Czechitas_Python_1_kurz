b6983e93da65d3c468baa9c474b1d922c3e6dc72
ypsilonx <cibulec.tom@gmail.com>
2022-09-21 17:47:51 +0200

    přidání knihy do seznamu

--- slovniky_cykly_lekce.py
+++ slovniky_cykly_lekce.py
@@ -2,21 +2,22 @@ sales = {
     "Zkus mě chytit": 4165,
     "Vrah zavolá v deset": 5681,
     "Zločinný steh": 2565,
+    "Ucho belzebuba": 3584,
 }
 
-# for key in sales:
-#     print(key)
+for key in sales:
+    print(key)
     
-# for key, value in sales.items():
-#     print("Knihy", key, "bylo prodáno", value, "výtisků.")
-#     # Použití f-stringu
-#     print(f"Knihy {key} bylo prodáno {value} výtisků.")
+for key, value in sales.items():
+    print("Knihy", key, "bylo prodáno", value, "výtisků.")
+    # Použití f-stringu
+    print(f"Knihy {key} bylo prodáno {value} výtisků.")
 
-# total_sales = 0
-# for key, value in sales.items():
-#     # print("Knihy", key, "bylo prodáno", value, "výtisků.")
-#     # Použití f-stringu
-#     print(f"Knihy {key} bylo prodáno {value} výtisků.")
-#     total_sales += value
-# print(f"Celkem bylo prodáno {total_sales} výtisků.")
+total_sales = 0
+for key, value in sales.items():
+    # print("Knihy", key, "bylo prodáno", value, "výtisků.")
+    # Použití f-stringu
+    print(f"Knihy {key} bylo prodáno {value} výtisků.")
+    total_sales += value
+print(f"Celkem bylo prodáno {total_sales} výtisků.")
 
