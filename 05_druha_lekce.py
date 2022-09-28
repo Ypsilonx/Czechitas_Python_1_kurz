26495470933ef87208b3196652a06031e15a6c7b
ypsilonx <cibulec.tom@gmail.com>
2022-09-28 21:44:27 +0200

    cvičení spolubydlící hotovo

--- 05_druha_lekce.py
+++ 05_druha_lekce.py
@@ -65,3 +65,43 @@
 #     if 500 <= item["price"] <= 1000 and item["in_stock"]:
 #         print(f"Vybraný dárek je {item['title']}")
 #         break
+
+purchase_list = [
+    {"person": "Petr", "item": "Prací prášek", "value": 399},
+    {"person": "Ondra", "item": "Savo", "value": 80},
+    {"person": "Petr", "item": "Toaletní papír", "value": 65},
+    {"person": "Libor", "item": "Pivo", "value": 124},
+    {"person": "Petr", "item": "Pytel na odpadky", "value": 75},
+    {"person": "Míša", "item": "Utěrky na nádobí", "value": 130},
+    {"person": "Ondra", "item": "Toaletní papír", "value": 120},
+    {"person": "Míša", "item": "Pečící papír", "value": 30},
+    {"person": "Zuzka", "item": "Savo", "value": 80},
+    {"person": "Pavla", "item": "Máslo", "value": 50},
+    {"person": "Ondra", "item": "Káva", "value": 300},
+]
+
+sum_per_person = {}
+for item in purchase_list:
+    person = item["person"]
+    value = item["value"]
+    if person in sum_per_person:
+        sum_per_person[person] += value
+    else:
+        sum_per_person[person] = value
+
+total_value = 0
+for person, value in sum_per_person.items():
+    total_value += value
+    print(f"{person} utratil(a) za společné nákupy {value} Kč.")
+
+average_value = total_value / len(sum_per_person)
+print(f"Průměrná hodnota na osobu je {round(average_value)} Kč.")
+
+# cvičení "Spolubydlící":
+
+for person, value in sum_per_person.items():
+    rozdil = value - average_value
+    if rozdil > 0:
+        print(f'{person} by měl dostat {round(rozdil)},- Kč.')
+    else:
+        print(f'{person} by měl doplatit {-(round(rozdil))},- Kč.')
