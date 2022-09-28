1b961cd7ce1b4f64b2401583e84ec2748f82f2ef
Ypsilonx <109464813+Ypsilonx@users.noreply.github.com>
2022-09-28 07:28:48 +0200

    Add files via upload

---
+++ 06_cykly_range_cviceni.py
@@ -0,0 +1,23 @@
+# 1. Tombola
+# V tombole bylo prodáno celkem 1000 lístků. Naším úkolem je vylosovat náhodně tři výherce. Napište program, který vygeneruje a vypíše tři čísla mezi 1 a 1000. Využijte funkci *randint*, nezapomeňte ale, že si ji musíte importovat z modulu *random*.
+# Neřešte, že jedno číslo může být vygenerováno dvakrát.
+
+# TODO
+
+# 2. Dělitelnost více čísly
+# Vypišme si čísla z nějakého rozsahu na základě jejich dělitelnosti dvěma čísly.
+# a) Zkuste z nějakého rozsahu čísel vypsat čísla, která jsou dělitelná 3 i 4 současně.
+# b) Zkuste z nějakého rozsahu čísel vypsat čísla, která jsou dělitelná 5 nebo 6. Stačí vypsat text: "Číslo je dělitelné 5 nebo 6."
+
+# TODO
+
+# 3. Seznam hostů na party
+# Vraťte se k příkladu se zadáváním seznamu hostu na párty a zkopírujte si kód k sobě do editoru. Upravte program tak, že pokud uživatel v průběhu zadávání jmen napíše "konec", cyklus na zadávání jmen se ukončí.
+
+# TODO
+
+# BONUS
+# Požádej uživatele o zadání celého čísla. Následně urči, zda je toto číslo prvočíslo.
+# Prvočíslo je číslo, které je dělitelné beze zbytku pouze 2 čísly - 1 a sebou samotným.
+# - Například 5 je prvočíslo, protože je dělitelná pouze 1 a 5.
+# - Naopak 4 není prvočíslo, protože je dělitelná 1, 2 a 4.
