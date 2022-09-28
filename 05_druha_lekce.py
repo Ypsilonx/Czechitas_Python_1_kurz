fa84f30000aeb9913a0d592f19c634f4eb4d4b59
ypsilonx <cibulec.tom@gmail.com>
2022-09-28 21:14:22 +0200

    cvičení hotovo

--- 05_druha_lekce.py
+++ 05_druha_lekce.py
@@ -28,3 +28,40 @@
 
 # for item in 'Andrea':
 #     print(item)
+
+# stop = int(input("Zadej konec: "))
+# for i in range(stop):
+#     print(i)
+
+# start = int(input("Zadej začátek: "))
+# stop = int(input("Zadej konec: "))
+# for i in range(start, stop):
+#     print(i)
+
+# start = int(input("Zadej začátek: "))
+# stop = int(input("Zadej konec: "))
+# for i in range(start, stop):
+#     if i % 3 == 0:
+#         print(i)
+
+# number_of_guests = int(input("Zadej počet hostů: "))
+# guest_list = []
+# for i in range(number_of_guests):
+#     new_guest = input("Zadej jméno hosta: ")
+#     guest_list.append(new_guest)
+# print(guest_list)
+
+# list_of_items = [
+#     {"title": "Modrá kravata", "price": 239, "in_stock": True},
+#     {"title": "Luxusní psací pero", "price": 1599, "in_stock": True},
+#     {"title": "Degustační balíček kávy", "price": 599, "in_stock": True},
+#     {"title": "Parfém", "price": 559, "in_stock": False},
+#     {"title": "Čajová konvička s hrnky", "price": 899, "in_stock": True},
+#     {"title": "Sklenice na víno", "price": 799, "in_stock": True},
+#     {"title": "Fitness náramek", "price": 2399, "in_stock": False},
+# ]
+
+# for item in list_of_items:
+#     if 500 <= item["price"] <= 1000 and item["in_stock"]:
+#         print(f"Vybraný dárek je {item['title']}")
+#         break
--- 06_cykly_range_cviceni.py
+++ 06_cykly_range_cviceni.py
@@ -1,23 +1,83 @@
 # 1. Tombola
-# V tombole bylo prodáno celkem 1000 lístků. Naším úkolem je vylosovat náhodně tři výherce. Napište program, který vygeneruje a vypíše tři čísla mezi 1 a 1000. Využijte funkci *randint*, nezapomeňte ale, že si ji musíte importovat z modulu *random*.
+# V tombole bylo prodáno celkem 1000 lístků. Naším úkolem je vylosovat náhodně tři výherce. 
+# Napište program, který vygeneruje a vypíše tři čísla mezi 1 a 1000. Využijte funkci *randint*, nezapomeňte ale, že si ji musíte importovat z modulu *random*.
 # Neřešte, že jedno číslo může být vygenerováno dvakrát.
 
-# TODO
+# import random
+
+# prodane_listky = int(input('Zadej kolik se prodalo lístků: '))
+# for i in range(1, 4):
+#     vyherni_los = random.randint(1, (prodane_listky + 1))
+#     print(f'{i}. výherní los je číslo {vyherni_los}.')
 
 # 2. Dělitelnost více čísly
 # Vypišme si čísla z nějakého rozsahu na základě jejich dělitelnosti dvěma čísly.
 # a) Zkuste z nějakého rozsahu čísel vypsat čísla, která jsou dělitelná 3 i 4 současně.
 # b) Zkuste z nějakého rozsahu čísel vypsat čísla, která jsou dělitelná 5 nebo 6. Stačí vypsat text: "Číslo je dělitelné 5 nebo 6."
 
-# TODO
+# a)
+# rozsah_start = int(input('Zadej začátek rozsahu: '))
+# rozsah_konec = int(input('Zadej konec rozsahu: '))
+# delitel = [3, 4]
+# for cislo in range(rozsah_start, rozsah_konec):
+#     if cislo % delitel[0] == 0 and cislo % delitel[1] == 0:
+#         print(cislo)
+# b)
+# rozsah_start = int(input('Zadej začátek rozsahu: '))
+# rozsah_konec = int(input('Zadej konec rozsahu: '))
+# delitel = [(int(input('Zadej dělitele číslo 1: '))), (int(input('Zadej dělitele číslo 2: ')))]
+# for cislo in range(rozsah_start, rozsah_konec):
+#     if cislo % delitel[0] == 0 or cislo % delitel[1] == 0:
+#         print(f'Čísla jsou dělitelná {delitel[0]} a {delitel[1]}.')
+#         break
 
 # 3. Seznam hostů na party
-# Vraťte se k příkladu se zadáváním seznamu hostu na párty a zkopírujte si kód k sobě do editoru. Upravte program tak, že pokud uživatel v průběhu zadávání jmen napíše "konec", cyklus na zadávání jmen se ukončí.
+# Vraťte se k příkladu se zadáváním seznamu hostu na párty a zkopírujte si kód k sobě do editoru. 
+# Upravte program tak, že pokud uživatel v průběhu zadávání jmen napíše "konec", cyklus na zadávání jmen se ukončí.
 
-# TODO
+# number_of_guests = int(input("Zadej počet hostů: "))
+# guest_list = []
+# for i in range(number_of_guests):
+#     new_guest = input("Zadej jméno hosta: ")
+#     if new_guest == 'konec':
+#         break
+#     guest_list.append(new_guest)
+# print(guest_list)
 
 # BONUS
 # Požádej uživatele o zadání celého čísla. Následně urči, zda je toto číslo prvočíslo.
 # Prvočíslo je číslo, které je dělitelné beze zbytku pouze 2 čísly - 1 a sebou samotným.
 # - Například 5 je prvočíslo, protože je dělitelná pouze 1 a 5.
 # - Naopak 4 není prvočíslo, protože je dělitelná 1, 2 a 4.
+
+# zadane_cislo = int(input('Zadej jakékoliv číslo: '))
+# delitele = []
+# prvocislo = True
+# for delitel in range(1, (zadane_cislo + 1)):
+#     if 1 < (zadane_cislo / delitel) < zadane_cislo and (zadane_cislo % delitel == 0):
+#         delitele.append(delitel)
+#         print(f'{delitele}')
+# if zadane_cislo > 1:    
+#     for delitel in range(1, (zadane_cislo + 1)):
+#         if (zadane_cislo % delitel) == 0:
+#             prvocislo = False
+#             break
+# if prvocislo:
+#     print(f'{zadane_cislo} je prvočíslo, protože je dělitelné 1 a {zadane_cislo}.')
+# else:
+#     print(f'{zadane_cislo} není prvočíslo, protože je dělitelné 1, {delitele} a {zadane_cislo}.')
+
+# od lektorky:
+
+# zadane_cislo = int(input("Zadej cislo: "))
+# je_prvocislo = True
+
+# if num > 1:
+#     for i in range(2, zadane_cislo):
+#         if (num % i) == 0:
+#             je_prvocislo = False
+#             break
+# if je_prvocislo:
+#     print(f'{zadane_cislo} je prvočíslo.')
+# else:
+#     print(f'{zadane_cislo} není prvočíslo.')
