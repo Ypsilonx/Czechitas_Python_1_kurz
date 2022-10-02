90bfdb547f130914215ddc73e5708c89d5df9cc3
ypsilonx <cibulec.tom@gmail.com>
2022-10-02 09:39:27 +0200

    ...

--- 06_cykly_range_cviceni.py
+++ 06_cykly_range_cviceni.py
@@ -91,6 +91,11 @@
 
 # cvičení WHILE:
 
+# Hledání čísla:
+# Napište interaktivní hru, ve které počítač vygeneruje tajné číslo v rozsahu 1 až 100 (použijte funkci random.randint()). 
+# Následně se v cyklu ptejte uživatele, aby zadal číslo a vypisujte vždy jestli je zadané číslo nižší nebo vyšší než tajné číslo. 
+# Ukončete cyklus v momentě, kdy uživatel trefí tajné číslo.
+
 #import random
 
 # zadane_cislo = int(input("Zkus trefit tajné číslo: "))
