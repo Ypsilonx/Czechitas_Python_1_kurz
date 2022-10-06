a0ebc6da6758a273fc3c01779f2d63d3c9ee7403
ypsilonx <cibulec.tom@gmail.com>
2022-10-06 20:19:09 +0200

    ....

--- 10_ukol_03.py
+++ 10_ukol_03.py
@@ -5,9 +5,12 @@
 #Zeptá se uživatele na zprávu, kterou chce zaslat. Následně vypíše, kolik zpráva bude stát.
 #Tvůj program bude obsahovat dvě funkce:
 
-#První funkce ověří délku telefonního čísla. Uvažuj, že telefonní číslo může být devítimístné nebo třináctimístné (pokud je na začátku předvolba "+420"). Nemusíte kontrolovat, zda uživatel zadal skutečně číslo, či zda jsou tam i jiné znaky. To jsme v kurzu zatím neřešili. Pokud je číslo platné, funkce vrátí hodnotu True, jinak vrátí hodnotu False.
+#První funkce ověří délku telefonního čísla. Uvažuj, že telefonní číslo může být devítimístné nebo třináctimístné (pokud je na začátku předvolba "+420"). 
+#Nemusíte kontrolovat, zda uživatel zadal skutečně číslo, či zda jsou tam i jiné znaky. To jsme v kurzu zatím neřešili. 
+#Pokud je číslo platné, funkce vrátí hodnotu True, jinak vrátí hodnotu False.
 #Druhá funkce spočte cenu zprávy. Uživatel platí 3 Kč za každých započatých 180 znaků.
-#Tvůj program nejprve ověří pomocí první funkce správnost telefonního čísla. Pokud není platné, vypíše chybu, v opačném případě se zeptá na text zprávy a pomocí druhé funkce určí její cenu, kterou vypíše uživateli.
+#Tvůj program nejprve ověří pomocí první funkce správnost telefonního čísla. 
+# Pokud není platné, vypíše chybu, v opačném případě se zeptá na text zprávy a pomocí druhé funkce určí její cenu, kterou vypíše uživateli.
 
 #Nápověda
 #Pokud chcete zkontrolovat předvolbu, stačí využít podmínku+420 in cislo, alternativně můžete využít indexy: Python umožňuje kromě jednoho znaku z řetězce získat i více znaků, a to pomocí dvojtečky. Pokud budete chtít získat první čtyři znaky, napište cislo[0:4]. Pak můžete vytvořit podmínku cislo[0:4] == "+420".
