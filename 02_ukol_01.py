4f626a8b76b0534673d3ffb15e65361d8a198837
ypsilonx <cibulec.tom@gmail.com>
2022-10-09 17:33:24 +0200

    doplnění kodu

--- 02_ukol_01.py
+++ 02_ukol_01.py
@@ -1,10 +1,10 @@
-# baliky = {
-#     "B541X": True,
-#     "B547X": False,
-#     "B251X": False,
-#     "B501X": True,
-#     "B947X": False,
-# }
+baliky = {
+    "B541X": True,
+    "B547X": False,
+    "B251X": False,
+    "B501X": True,
+    "B947X": False,
+}
 # kod_baliku = ''
 # while kod_baliku not in baliky:
 #     kod_baliku = input('Zadej kód balíku: ')
@@ -15,12 +15,12 @@
 #     else:
 #         print('Zadal si špatné číslo balíku.')
 
-# cislo_baliku = input('Zadej kód balíku: ')
-# if cislo_baliku in baliky:
-#     na_sklade = baliky[cislo_baliku]
-#     if na_sklade == True:
-#         print('Balík byl předán kurýrovi.')
-#     else:
-#         print('Balík nebyl ještě předán kurýrovi.')
-# else:
-#     print('Tento balík není v přepravě.')
+cislo_baliku = input('Zadej kód balíku: ')
+if cislo_baliku in baliky:
+    na_sklade = baliky[cislo_baliku]
+    if na_sklade == True:
+        print('Balík byl předán kurýrovi.')
+    else:
+        print('Balík nebyl ještě předán kurýrovi.')
+else:
+    print(f'Balík {cislo_baliku} není v přepravě.')
