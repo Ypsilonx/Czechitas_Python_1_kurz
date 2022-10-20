0d367495e4c979036022ed50340f2ee5b3b76d78
ypsilonx <cibulec.tom@gmail.com>
2022-10-20 15:18:24 +0200

    ...

--- 13_ukol_4.py
+++ 13_ukol_4.py
@@ -35,10 +35,16 @@ class Kucharka:
     
     def __str__(self):
         if len(self.recepty) > 0:
-            pocet_recept = f'{Kucharka.pocet_receptu(self)} recep.'
+            pocet_recept = f'{Kucharka.pocet_receptu(self)} recep. - seznam:'
         else:
             pocet_recept = 'neobsahuje recepty'
-        return f'{self.nazev} od {self.autor} - {pocet_recept} - seznam: {[Recept.nazev for Recept in self.recepty]}'
+        vypis = f'{self.nazev} od {self.autor} - {pocet_recept}'
+        print(vypis)
+        seznam = [[recept.nazev, recept.narocnost, recept.url_adresa] for recept in self.recepty]
+        for recep in seznam:
+            seznam_recep = f'Recept: {recep[0]} /náročnost: {recep[1]} /adresa: {recep[2]}'
+            print(seznam_recep)
+        return ''
     
     def pocet_receptu(self):
         return len(self.recepty)
@@ -47,25 +53,29 @@ class Kucharka:
         return self.recepty.append(recept)
     
     def vyzkousene_recepty(self):
-        return [Recept.nazev for Recept in self.recepty if Recept.vyzkouseno]
+        return [zkouska.nazev for zkouska in self.recepty if zkouska.vyzkouseno]
 
         
 muj_recept1 = Recept('Babovka', 1, 'babovky.cz')
 muj_recept2 = Recept('Štrůdl', 2, 'strudly.eu')
 muj_recept3 = Recept('Muffin', 2, 'muffins.com')
+muj_recept4 = Recept('Buchta', 5, 'buchty.cz')
 muj_recept1.zmen_narocnost(3)
 muj_recept1.zkusit()
 print(muj_recept1)
 print(muj_recept2)
 print(muj_recept3)
+muj_recept4.zkusit()
+print(muj_recept4)
 print('___________________________________')
 moje_kucharka1 = Kucharka('Domací kuchařka', 'Tom')
 print(moje_kucharka1)
 moje_kucharka1.pridej_recept(muj_recept1)
+muj_recept2.zmen_narocnost(5)
 moje_kucharka1.pridej_recept(muj_recept2)
 moje_kucharka1.pridej_recept(muj_recept3)
+moje_kucharka1.pridej_recept(muj_recept4)
 print(moje_kucharka1)
 print('___________________________________')
 
-muj_recept2.zkusit()
 print(moje_kucharka1.vyzkousene_recepty())
